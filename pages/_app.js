600ba14d80a9a3444cd9a1fbbf653d16fc98782a
stanely86 <stanely8662@gmail.com>
2025-08-05 17:56:49 +0800

    Added GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,34 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";         // ✅ Required for <Head>
+import Script from "next/script";     // ✅ Required for GA4 scripts
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
       <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8D0G1C9W75"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-8D0G1C9W75');
-            `,
-          }}
-        />
+        <title>Stanley Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      {/* GA4 Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-8D0G1C9W75"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="ga4-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8D0G1C9W75');
+          `,
+        }}
+      />
+
       <Component {...pageProps} />
     </ThemeProvider>
   );
