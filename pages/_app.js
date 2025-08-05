f10ad837246169c2941911ccc8fdc1d57e29116e
stanely86 <stanely8662@gmail.com>
2025-08-05 17:48:39 +0800

    Added GA4

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,20 @@ import { ThemeProvider } from "next-themes";
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8D0G1C9W75"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8D0G1C9W75');
+            `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
